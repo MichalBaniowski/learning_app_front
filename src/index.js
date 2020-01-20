9c4b5355dc04f680a574da2bd73e5b393fa30c5b
Michał Baniowski <michal.baniowski88@gmail.com>
2020-01-20 21:40:05 +0100

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./components/global/App";
+import '../css/style.scss';
+
+ReactDOM.render(<App/>, document.getElementById('app'));
